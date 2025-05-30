4f68b0a2e95d3eefead999a6d5a832398d3157bd
TN <amezaikupan@gmail.com>
2025-05-30 22:54:13 +0700

    Fix: File path

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 list_num = [1, 2, 3, 4, 5]
-with open('/home/tn/git/tests/test_git_action/output.txt', 'w') as f:
+with open('output.txt', 'w') as f:
     for num in list_num:
         f.write(f"{num}\n")
