bf1ad9ef1170c2ca884d1754546bcd091dd8b561
TN <amezaikupan@gmail.com>
2025-05-30 22:52:28 +0700

    Fix: Remove dependencies

--- test.py
+++ test.py
@@ -1,6 +1,4 @@
-import numpy as np
-
-list_num = np.random.randint(0, 100, size=10).tolist()
+list_num = [1, 2, 3, 4, 5]
 with open('/home/tn/git/tests/test_git_action/output.txt', 'w') as f:
     for num in list_num:
         f.write(f"{num}\n")
